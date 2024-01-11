f1d8dbe584413fbebaa9e0357d3e92f087196d85
RiccardoImperato <riccardo.imperato@libero.it>
2024-01-11 17:48:13 +0100

    feat: stile pagina e font monserrat

--- index.php
+++ index.php
@@ -5,23 +5,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Dischi</title>
+    <!-- Font Montserrat -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    <!--/ Font Montserrat -->
     <!-- Style -->
     <link rel="stylesheet" href="./css/style.css">
     <!--/ Style -->
 </head>
 
 <body>
-    <h1>Albums</h1>
-    <div id="app">
-        <ul>
-            <li v-for="album in albums">
-                <img :src="album.image" :alt="album.title">
-                <h3>{{ album.title }}</h3>
-                <div>{{ album.artist }}</div>
-                <div>{{ album.year }}</div>
-            </li>
-        </ul>
-    </div>
+    <header>
+        <div class="container">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1982px-Spotify_icon.svg.png" alt="Spotify">
+        </div>
+    </header>
+    <main>
+        <div id="app">
+            <div class="container">
+                <ul>
+                    <li v-for="album in albums">
+                        <img :src="album.image" :alt="album.title">
+                        <div class="info">
+                            <h3>{{ album.title }}</h3>
+                            <div>{{ album.artist }}</div>
+                            <div>{{ album.year }}</div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </main>
     <!-- Vue js -->
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <!--/ Vue js -->
