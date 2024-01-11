6b2b38a2aa52588f17a31f80c856bd7e7fa8ead9
RiccardoImperato <riccardo.imperato@libero.it>
2024-01-11 15:14:36 +0100

    feat: first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dischi</title>
+    <!-- Style -->
+    <link rel="stylesheet" href="./css/style.css">
+    <!--/ Style -->
+</head>
+
+<body>
+
+    <!-- Vue js -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!--/ Vue js -->
+    <!-- Axios -->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <!--/ Axios -->
+</body>
+
+</html>
